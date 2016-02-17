da85bcb17c2e546c39d5a64e8a5667ed9ac77f94
Lucas Vieira <lucas.vieira@sirius.com.br>
2016-02-17 17:03:31 -0200

    Funções e declaração p1

---
+++ desafio04.js
@@ -0,0 +1,11 @@
+/*
+	- Desafio Número 4
+	-- Crie um código que informe se um usuário é maior de idade, e depois retorne uma mensagem "Você é maior de idade" ou ""
+*/
+
+var usuario = {
+	[
+		nome: "Lucas",
+		idade : 20
+	]
+}
